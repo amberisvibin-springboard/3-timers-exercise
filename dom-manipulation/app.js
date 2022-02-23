356e8feed71adbf282904e0177e2ad0dfd7823ed
Amber Zeller <amberisvibin@users.noreply.github.com>
2022-02-22 22:34:40 -0500

    finish project

--- dom-manipulation/app.js
+++ dom-manipulation/app.js
@@ -17,5 +17,48 @@ function selectWithClassThirdInsideOl() {
 function addHelloToIDContainer() {
     const container = document.querySelector("#container");
     const hello = document.createElement("p");
-    
+    hello.innerText = "Hello!";
+    container.append(hello);
+}
+
+function addClassMainToDivWithClassFooter() {
+    const footer = document.querySelector("div.footer");
+    footer.classList.add("main");
+}
+
+function removeClassMainToDivWithClassFooter() {
+    const footer = document.querySelector("div.footer");
+    footer.classList.remove("main");
+}
+
+function liThings() {
+    const li = createLi();
+    giveElementTextFour(li);
+    appendElementToUl(li);
+    makeAllLisInOlGreen();
+}
+
+function createLi() {
+    return document.createElement("li");
+}
+
+function giveElementTextFour(element) {
+    element.innerText = "four";
+}
+
+function appendElementToUl(element) {
+    const ul = document.querySelector("ol");
+    ul.append(element);
+}
+
+function makeAllLisInOlGreen() {
+    const lis = document.querySelectorAll("ol li");
+    for(let li of lis){
+        li.style.backgroundColor = "green";
+      }
+}
+
+function removeDivWithClassFooter() {
+    const footer = document.querySelector("div.footer");
+    footer.remove();
 }
