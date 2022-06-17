a8a9322fab856b4788e0344ce891c16586d151bb
Amber Zeller <amberisvibin@users.noreply.github.com>
2022-06-17 13:34:41 -0400

    finish flask-blogly

--- flask-blogly/app.py
+++ flask-blogly/app.py
@@ -1,9 +1,10 @@
 """Blogly application."""
 
+from curses.ascii import US
 from flask import Flask, render_template, request, make_response, redirect, flash, session, jsonify
 from werkzeug import exceptions
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -17,27 +18,76 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 db.create_all()
 
+
 @app.route("/")
 def index():
     """Show homepage"""
     return render_template("index.html")
 
+
 @app.route("/users")
 def users():
     """Show user list"""
-    return render_template("users.html")
+    user_list = User.query.all()
+    return render_template("users.html", users=user_list)
 
-@app.route("/users/user")
-def user():
+
+@ app.route("/users/<int:user_id>")
+def show_user(user_id):
     """Show user"""
-    return render_template("user.html", first_name="John", last_name="Doe", image_url="/static/default.jpg")
+    user = User.query.get_or_404(user_id)
+    return render_template("user.html", user=user)
 
-@app.route("/users/create")
-def create():
-    """Show create user form"""
-    return render_template("create.html")
 
-@app.route("/users/edit")
-def edit():
+@ app.route("/users/new")
+def new():
+    """Show new user form"""
+    return render_template("new.html")
+
+
+@app.route("/users/new", methods=["POST"])
+def new_submit():
+    """Get new user and redirect to users"""
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    new_user = User(first_name=first_name,
+                    last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect("/users")
+
+
+@ app.route("/users/<int:user_id>/edit")
+def edit(user_id):
     """Show edit user form"""
-    return render_template("edit.html")
+    user = User.query.get_or_404(user_id)
+    return render_template("edit.html", user=user)
+
+
+@ app.route("/users/<int:user_id>/edit", methods=["POST"])
+def get_edit(user_id):
+    """Get edit user form"""
+
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    user = User.query.get_or_404(user_id)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+    db.session.commit()
+    return redirect(f"/users/{user_id}")
+
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete(user_id):
+    """Delete user and redirect to users"""
+
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect("/users")
--- flask-blogly/models.py
+++ flask-blogly/models.py
@@ -10,12 +10,20 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+
 class User(db.Model):
     """User"""
 
     __tablename__ = "users"
 
-    id = db.Columm = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Columm = db.Column(
+        db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    image_url = db.Column(db.String(50), nullable=False)
+    image_url = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        """Show info about user."""
+
+        p = self
+        return f"<User {p.id} {p.first_name} {p.last_name} {p.image_url}>"
