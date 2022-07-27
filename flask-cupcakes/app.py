bdd758560a1e0e8e7ebb7bde7f4f787c55a97445
Amber <63863236+amberisvibin@users.noreply.github.com>
2022-07-27 10:21:34 -0400

    GET/POST routes on flask-cupcakes

--- flask-cupcakes/app.py
+++ flask-cupcakes/app.py
@@ -1,1 +1,87 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, jsonify, json, abort, request
+from models import db, connect_db, Cupcake
+from werkzeug.exceptions import HTTPException, NotFound
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "transgenderism"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+connect_db(app)
+db.create_all()
+
+# code from SO to make errors return as JSON
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
+
+def serialize_cupcake(cupcake):
+    """Serialize a cupcake SQLAlchemy obj to dictionary."""
+
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "size": cupcake.size,
+        "rating": cupcake.rating,
+        "image": cupcake.image,
+    }
+
+@app.route("/api/cupcakes")
+def list_all_cupcakes():
+    """Return JSON {'cupcakes': [{id, flavor, size, rating, image}, ...]}"""
+
+    cupcakes = Cupcake.query.all()
+
+    serialized = [serialize_cupcake(c) for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.route("/api/cupcakes/<cupcake_id>")
+def list_single_cupcake(cupcake_id):
+    """Return JSON {'cupcake': {id, flavor, size, rating, image}}"""
+
+    cupcake = Cupcake.query.get(cupcake_id)
+
+    if (not cupcake):
+        raise NotFound
+
+    serialized = serialize_cupcake(cupcake)
+
+    return jsonify(cupcake=serialized)
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """Create cupcake from form data & return it.
+
+    Returns JSON {'cupcake': {id, flavor, size, rating, image}}
+    """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+    
+
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = serialize_cupcake(new_cupcake)
+
+    # Return w/status code 201 --- return tuple (json, status)
+    return ( jsonify(cupcake=serialized), 201 )
--- flask-cupcakes/models.py
+++ flask-cupcakes/models.py
@@ -1,1 +1,29 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Columm = db.Column(
+        db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.String(50), nullable=False)
+    size = db.Column(db.String(50), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.String(200))
+
+    def __repr__(self):
+        """Show info about cupcake."""
+
+        c = self
+        return f"<Cupcake {c.id}>"
