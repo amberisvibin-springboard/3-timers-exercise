d770178a51971f2bef188d20a97e0097e6c4c2a0
Amber <63863236+amberisvibin@users.noreply.github.com>
2022-10-19 17:16:31 -0400

    work on express-messagely

--- express-messagely/models/user.js
+++ express-messagely/models/user.js
@@ -1,6 +1,7 @@
 /** User class for message.ly */
 
-
+const db = require("../db");
+const ExpressError = require("../expressError");
 
 /** User of the site. */
 
@@ -10,7 +11,22 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+    const result = await db.query(
+      `INSERT INTO users (
+            username, 
+            password, 
+            first_name, 
+            last_name, 
+            phone,
+            join_at,
+            last_login_at)
+          VALUES ($1, $2, $3, $4, $5, current_timestamp, current_timestamp)
+          RETURNING username, password, first_name, last_name, phone`,
+      [username, password, first_name, last_name, phone]);
+
+  return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
@@ -18,12 +34,31 @@ class User {
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+         SET last_login_at = current_timestamp
+         WHERE username = $1
+         RETURNING username, last_login_at`,
+      [username]);
+
+  if (!result.rows[0]) {
+    throw new ExpressError(`No such user: ${username}`, 404);
+  }
+
+  return result.rows[0];
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+  static async all() {
+    const result = await db.query(
+        `SELECT username, first_name, last_name, phone
+          FROM users`);
+
+    return result.rows;
+  }
 
   /** Get: get user by username
    *
@@ -34,7 +69,27 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+  static async get(username) {
+    const result = await db.query(
+        `SELECT 
+            username, 
+            first_name, 
+            last_name, 
+            phone, 
+            join_at, 
+            last_login_at
+          FROM users
+          WHERE username = $1`,
+        [username]);
+
+    let u = result.rows[0];
+
+    if (!u) {
+      throw new ExpressError(`No such user: ${username}`, 404);
+    }
+    
+    return u;
+  }
 
   /** Return messages from this user.
    *
@@ -48,7 +103,7 @@ class User {
 
   /** Return messages to this user.
    *
-   * [{id, from_user, body, sent_at, read_at}]
+   * [{id, from_user, body, sent_at, read_at}]l
    *
    * where from_user is
    *   {username, first_name, last_name, phone}
