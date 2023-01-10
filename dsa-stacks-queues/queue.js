d8337ec7583f3775f1d38168445182b65cf52a55
Amber <63863236+amberisvibin@users.noreply.github.com>
2023-01-09 21:25:24 -0500

    dsa-stacks-queues

--- dsa-stacks-queues/queue.js
+++ dsa-stacks-queues/queue.js
@@ -20,26 +20,51 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    let node = new Node(val);
+    if (!this.first) {
+      this.first = node;
+    }
+    if (this.last) {
+      this.last.next = node;
+    }
+    this.last = node;
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    let node = this.first;
+    // switch statements my beloved
+    switch (this.size) {
+      case 0:
+        throw new Error("Cannot dequeue() empty queue.");
+      case 1:
+        this.first = null;
+        this.last = null;
+      default:
+        this.first = node.next;
+    }
+    this.size--;
+    return node.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    switch (this.size) {
+      case 0:
+        throw new Error("Cannot peek() empty queue.");
+      default:
+        return this.first.val;
+    }
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return !this.size;
   }
 }
 
--- dsa-stacks-queues/stack.js
+++ dsa-stacks-queues/stack.js
@@ -17,29 +17,52 @@ class Stack {
     this.size = 0;
   }
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
+  /** push(val): add new value to start of the stack. Returns undefined. */
 
   push(val) {
-
+    let node = new Node(val);
+    if (!this.last) {
+      this.last = node;
+    }
+    node.next = this.first;
+    this.first = node;
+    this.size++;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    let node = this.first;
+    // switch statements my beloved
+    switch (this.size) {
+      case 0:
+        throw new Error("Cannot pop() empty stack.");
+      case 1:
+        this.first = null;
+        this.last = null;
+      default:
+        this.first = node.next;
+    }
+    this.size--;
+    return node.val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    switch (this.size) {
+      case 0:
+        throw new Error("Cannot peek() empty stack.");
+      default:
+        return this.first.val;
+    }
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return !this.size;
   }
 }
 
